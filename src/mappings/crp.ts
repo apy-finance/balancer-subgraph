a2c77c5b9b9c801d7463c6ba74fcf65c3154a386
opz <will.shahda@gmail.com>
2020-10-27 21:46:50 -0400

    Add CRP template

---
+++ src/mappings/crp.ts
@@ -0,0 +1,34 @@
+import { BigInt, Address, Bytes, store } from '@graphprotocol/graph-ts'
+import { LOG_CALL } from '../types/templates/CRP/ConfigurableRightsPool'
+import { CRP } from '../types/templates/CRP/ConfigurableRightsPool'
+import { Pool } from '../types/templates/CRP/Pool'
+import { PriceHistory } from '../types/schema'
+import {
+  hexToDecimal,
+  bigIntToDecimal,
+  tokenToDecimal,
+  createPoolShareEntity,
+  createPoolTokenEntity,
+  updatePoolLiquidity,
+  saveTransaction,
+  ZERO_BD,
+  decrPoolCount
+} from './helpers'
+
+/************************************
+ ********** CRP Controls ***********
+ ************************************/
+
+export function priceHistory(event: LOG_CALL): void {
+  let poolId = event.address.toHex()
+
+  //let crp = ConfigurableRightsPool.bind(event.address)
+  //let pool = Pool.bind(crp.bPool)
+
+  //priceHistory.price
+  priceHistory.timestamp = event.block.timestamp;
+  priceHistory.sequenceNumber = (event.block.timestamp * BigInt.fromI32(100000000)) + event.logIndex;
+  priceHistory.poolTokenAddress = event.address
+
+  saveTransaction(event, 'poke')
+}
